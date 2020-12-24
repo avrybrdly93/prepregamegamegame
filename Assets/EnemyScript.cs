607d1e84bf069d8ab3c70a62e6eac09f2cd543c1
Avery <cbra067@lasierra.edu>
2020-12-24 15:00:22 -0800

    initial commit - Christmas EVE!

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float speed = 3f;
+    public Rigidbody2D rb;
+    public LayerMask boundaryLayers;
+    public float waitTime = 0.03f;
+    public float timeUntilBoundary;
+    Vector2 movement = new Vector2(3f,0);
+
+    bool isFacingRight = true; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.centerOfMass = new Vector2(0, -1);
+        rb.velocity = movement;
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (!reachedBoundary())
+        {
+            //Keeps the enemy moving at a constant speed based on which way facing
+            facingRightFrustration();
+        }
+        else
+        {
+            isFacingRight = !isFacingRight;
+            facingRightFrustration();
+        }
+
+        Debug.Log(rb.velocity);
+
+        
+    }
+
+    public bool reachedBoundary()
+    {
+        Collider2D boundaryCheck = Physics2D.OverlapCircle(rb.position, 1f, boundaryLayers);
+        Debug.Log(boundaryCheck);
+        return boundaryCheck != null;
+    }
+
+    public void facingRightFrustration()
+    {
+        if (isFacingRight)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+    }
+}
+
+
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float mx = 10f;
+    public float movementSpeed;
+    public float jumpForce = 10f;
+
+    public Rigidbody2D rb;
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementSpeed = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+
+    public void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+}
